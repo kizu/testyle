00e3b526e1d0ed7dffd84fc6677c9df93fe961f0
Roman Komarov <kizu@yandex-team.ru>
2014-08-10 01:32:44 +0400

    Removed CSSO, added join-similar-rules (still failing tests)

--- testyle.js
+++ testyle.js
@@ -4,12 +4,12 @@
 
 var stylus = require('stylus');
 var fs = require('fs');
-var csso = require('csso');
 var autoprefixer = require('autoprefixer');
 var glob = require('glob');
-var Comb = require('csscomb');
+var CSScomb = require('csscomb');
+var JoinSimilarRules = require('csscomb-join-similar-rules');
 var whatToTest = process.env.npm_package_config_whatToTest || '**';
-var comb = new Comb();
+var comb = new CSScomb().use(JoinSimilarRules);
 comb.configure(require('./.csscomb.json'));
 
 // test cases
@@ -28,27 +28,8 @@ var Testyle = function(config) {
 
       style.render(function(err, actual){
         if (err) throw err;
-        // Change the order of csso and autoprefixer when
-        // we would able to set selector list code style
-        actual = csso.justDoIt(actual);
         actual = autoprefixer.process(actual).css;
         actual = comb.processString(actual);
-
-        // Remove those hardfixes when there would be a way to do this in csscomb
-        actual = actual.replace(/\)(,?)([^:\)\s,;])/g,')$1 $2');
-        actual = actual.replace(/,sans-serif/g,', sans-serif');
-        actual = actual.replace(/([^ ])\}/g,'$1}\n\n');
-        actual = actual.replace(/,#/g,', #');
-        actual = actual.replace(/,red/g,', red');
-        actual = actual.replace(/,transparent/g,', transparent');
-        actual = actual.replace(/background: 0 0/g,'background: transparent');
-        actual = actual.replace(/font-weight: 700/g,'font-weight: bold');
-        actual = actual.replace(/([^ ])!important/g,'$1 !important');
-        actual = actual.replace(/inset,([^ ])/g,'inset, $1');
-        actual = actual.replace(/\)rgba/g,') rgba');
-
-        // CSSO strips ie9 hack, should replace with smth else
-        actual = actual.replace(/color: #333 \\0\/;/g,'color: transparent;\n  color: #333 \\0/;');
 
         actual.trim().should.equal(css);
       });
